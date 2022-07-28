32540ac8b37a77ae39c655db570873532361f954
chencong1 <chencong1@joyy.com>
2022-07-28 16:27:35 +0800

    :sparkles: update config.js,add notes directory

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,11 +6,11 @@ module.exports = {
   dest: './dist',
   base: '/',
   head: [
-    ['meta', { name: 'Keywords', content: 'ccoderJava,ccoder,聪聪的日志' }],
-    ['meta', { name: 'description', content: '记录开发生活,ccoderJava,聪聪,ccoder' }],
-    ['link', { rel: 'icon', href: './favicon.ico' }],
+    ['meta', {name: 'Keywords', content: 'ccoderJava,ccoder,聪聪的日志'}],
+    ['meta', {name: 'description', content: '聪聪碎碎念,记录开发生活,ccoderJava,聪聪,ccoder'}],
+    ['link', {rel: 'icon', href: './favicon.ico'}],
     // google analytic
-    ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-40YT5YJMBG", async: true }],
+    ['script', {src: "https://www.googletagmanager.com/gtag/js?id=G-40YT5YJMBG", async: true}],
     ['script', {},
       ` window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
@@ -22,26 +22,31 @@ module.exports = {
                (function() {
                  var hm = document.createElement("script");
                  hm.src = "https://hm.baidu.com/hm.js?921719b1f1d6a98cb84343b54b11c7dd";
-                 var s = document.getElementsByTagName("script")[0]; 
+                 var s = document.getElementsByTagName("script")[0];
                  s.parentNode.insertBefore(hm, s);
                })();`],
   ],
-  title: '聪聪的日志',
-  description: '记录开发生活,ccoderJava,聪聪,ccoder',
+  title: '聪聪碎碎念',
+  description: '聪聪碎碎念,记录开发生活,ccoderJava,聪聪,ccoder',
   theme: '@vuepress/blog',
   themeConfig: {
     lastUpdated: "上次更新",
     dateFormat: 'YYYY-MM-DD hh:mm:ss',
     logo: './favicon.ico',
     nav: [
-      { text: '首页', link: '/' },
-      { text: '标签', link: '/tag/' },
-      { text: '个人简历', target: '_blank', link: 'https://ccoderjava.github.io/chencong_java software engineer_4 years.pdf' },
+      {text: '文章', link: '/', english: "study"},
+      {text: '随笔', link: '/notes/', english: "notes"},
+      {text: '标签', link: '/tag/', english: "tags"},
+      {
+        text: '个人简历',
+        target: '_blank',
+        link: 'https://ccoderjava.github.io/chencong_java software engineer_4 years.pdf'
+      },
     ],
     footer: {
       copyright: [
         {
-          text: 'Powered by VuePress | 鄂ICP备2021021487号-1',
+          text: '鄂ICP备2021021487号-1',
           link: 'https://beian.miit.gov.cn/',
         },
       ],
@@ -57,18 +62,34 @@ module.exports = {
         id: 'post',
         dirname: '_posts',
         path: '/',
-        itemPermalink: '/:slug'
+        itemPermalink: '/:slug',
+        pagination: {
+          prevText: '上一页',
+          nextText: '下一页',
+          lengthPerPage: 6,
+        },
       },
+      {
+        id: 'notes',
+        dirname: '_notes',
+        path: '/notes/',
+        itemPermalink: '/notes:slug',
+        pagination: {
+          prevText: '上一页',
+          nextText: '下一页',
+          lengthPerPage: 8,
+        },
+      }
     ],
     globalPagination: {
-      sorter: (prev, next) => {
-        const dayjs = require('dayjs')
-        const prevTime = dayjs(prev.frontmatter.date)
-        const nextTime = dayjs(next.frontmatter.date)
-        const prevTop = (prev.frontmatter.top || 0) * 10000000000000
-        const nextTop = (next.frontmatter.top || 0) * 10000000000000
-        return (prevTop || prevTime) - (nextTop || nextTime) > 0 ? -1 : 1
-      },
+      // sorter: (prev, next) => {
+      //   const dayjs = require('dayjs')
+      //   const prevTime = dayjs(prev.frontmatter.date)
+      //   const nextTime = dayjs(next.frontmatter.date)
+      //   const prevTop = (prev.frontmatter.top || 0) * 10000000000000
+      //   const nextTop = (next.frontmatter.top || 0) * 10000000000000
+      //   return (prevTop || prevTime) - (nextTop || nextTime) > 0 ? -1 : 1
+      // },
       prevText: '上一页', // Text for previous links.
       nextText: '下一页', // Text for next links.
       lengthPerPage: 8, // Maximum number of posts per page.
@@ -95,6 +116,8 @@ module.exports = {
     '@vuepress/active-header-links',
     '@markspec/vuepress-plugin-footnote',
     'vuepress-plugin-viewer',
-    '@vuepress/last-updated'
+    '@vuepress/last-updated', {
+      transformer: timestamp => timestamp
+    }
   ],
 }
---
+++ _notes/template.md
@@ -0,0 +1,9 @@
+---
+title: 模板文件template
+date: 2022-07-29 10:49:10
+author: 聪聪
+category: 旅途
+tags:
+- 旅途
+- 随笔
+---
