3837ad19d8f8d4def0daba8fa6109052b8debd3a
ccoderJava <congccoder@gmail.com>
2026-07-16 11:36:45 +0800

    refactor: 博客部署推送到 blog 仓库 gh-pages 分支

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,14 @@ set -e
 yarn run dist
 
 # 部署到 GitHub Pages
-TEMP_DIR=$(mktemp -d)
-git clone --depth 1 git@github.com:ccoderJava/ccoderJava.github.io.git "$TEMP_DIR"
-cd "$TEMP_DIR"
+cd dist
 
-# 更新 blog 目录
-rm -rf blog
-mkdir -p blog
-cp -r "$OLDPWD/dist/"* blog/
-cp "$OLDPWD/dist/CNAME" . 2>/dev/null || true
+echo 'blog.ccoder.cc' > CNAME
 
-# 提交推送
+git init
 git add -A
-git commit -m 'auto deploy' || true
-git push
+git commit -m 'auto deploy'
+
+git push -f git@github.com:ccoderJava/blog.git master:gh-pages
 
 cd -
-rm -rf "$TEMP_DIR"
