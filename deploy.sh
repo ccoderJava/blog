10307cd4df9080b19e1d2d471406ec88c1937040
ccoderJava <congccoder@gmail.com>
2026-07-16 11:24:13 +0800

    refactor: 部署产物推送到 ccoderJava.github.io 的 blog/ 子目录

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env sh
-
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
+# 构建
 yarn run dist
 
-# 进入生成的文件夹
-cd dist
+# 部署到 GitHub Pages
+TEMP_DIR=$(mktemp -d)
+git clone --depth 1 git@github.com:ccoderJava/ccoderJava.github.io.git "$TEMP_DIR"
+cd "$TEMP_DIR"
 
-# 如果是发布到自定义域名
-echo 'blog.ccoder.cc' > CNAME
+# 更新 blog 目录
+rm -rf blog
+mkdir -p blog
+cp -r "$OLDPWD/dist/"* blog/
+cp "$OLDPWD/dist/CNAME" . 2>/dev/null || true
 
-git init
+# 提交推送
 git add -A
-git commit -m 'auto deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:ccoderJava/ccoderJava.github.io.git master
+git commit -m 'auto deploy' || true
+git push
 
 cd -
+rm -rf "$TEMP_DIR"
